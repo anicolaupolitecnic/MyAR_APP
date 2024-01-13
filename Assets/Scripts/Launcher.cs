4a371082eacf4b8646f2bcab9dc6254a01bb6369
anicolaupolitecnic <anicolau@politecnicllevant.cat>
2024-01-13 16:02:57 +0100

    Initial commit

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private float force = 450.0f;
+    [SerializeField] private Transform pointer;
+
+    private Camera aRCamera;
+
+    void Start()
+    {
+        aRCamera = GameManager.Instance.ARCamera;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Vector3 inputPosition = Input.GetMouseButtonDown(0) ? Input.mousePosition : Input.GetTouch(0).position;
+
+            Ray ray = aRCamera.ScreenPointToRay(inputPosition);
+
+            Vector3 direction;
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                direction = hit.point - pointer.position;
+            } else
+            {
+                direction = pointer.forward;
+            }
+
+            GameObject ball = Instantiate(ballPrefab, pointer.position, Quaternion.identity, transform);
+            ball.GetComponent<Rigidbody>().AddForce(direction.normalized * force);
+        }
+    }
+}
---
+++ Assets/Scripts/MeshSpawner.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject aRPrefab;
+    [SerializeField] private float spawnInterval = 10.0f;
+
+    private Camera aRCamera;
+    private MeshRenderer meshRenderer;
+    private MeshFilter meshFilter;
+
+    private int minVertex = 250;
+    private float timeSinceLastSpawn = 11.0f;
+    private float normalHorizontalTolerance = 0.9f;
+
+    private float misDisYOrigin = 0.15f;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshFilter= GetComponent<MeshFilter>();
+        aRCamera= GameManager.Instance.ARCamera;
+    }
+
+    void Update()
+    {
+        int vertexCount = meshFilter.sharedMesh.vertexCount;
+        if (vertexCount < minVertex) 
+        {
+            return;
+        }
+
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (timeSinceLastSpawn >= spawnInterval && IsVisibleToCamera())
+        {
+            TryToSpawnARObject();
+        }
+    }
+
+    private bool IsVisibleToCamera()
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(aRCamera);
+        return GeometryUtility.TestPlanesAABB(planes, meshRenderer.bounds);
+    }
+
+    private void TryToSpawnARObject()
+    {
+        Mesh mesh = meshFilter.sharedMesh;
+
+        int indexVertex = Random.Range(0, mesh.vertexCount);
+
+        Vector3 normal = mesh.normals[indexVertex];
+
+        bool isHorizontal = normal.y > normalHorizontalTolerance;
+
+        Vector3 vertexPosition = mesh.vertices[indexVertex];
+
+        Vector3 globalPosition = transform.TransformPoint(vertexPosition);
+
+        bool isCloseToYOrigin = globalPosition.y < misDisYOrigin;
+
+        if (isHorizontal && isCloseToYOrigin)
+        {
+            Vector3 randomYRot = Vector3.up * Random.Range(0,360);
+
+            GameObject aRObject = Instantiate(aRPrefab, globalPosition, Quaternion.Euler(randomYRot), transform);
+            timeSinceLastSpawn = 0;
+        }
+
+    }
+}
